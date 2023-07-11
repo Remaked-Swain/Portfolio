323bf896d7c6c5d3222e1581ef337b3b818513a7
YunSwain <destap@naver.com>
2023-07-11 16:58:21 +0900

    Creation

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Swain Yun on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
