ecda84847b5676480cf520b608744c54dfd54755
YunSwain <destap@naver.com>
2023-08-13 19:17:39 +0900

    ConfirmationDialog

---
+++ Portfolio/SwiftUIComponents/Beginner/ConfirmationDialog/ConfirmationDialogBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ConfirmationDialogBootcamp.swift
+//  Portfolio
+//
+//  Created by Swain Yun on 2023/08/13.
+//
+
+import SwiftUI
+
+struct ConfirmationDialogBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConfirmationDialogBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfirmationDialogBootcamp()
+    }
+}
---
+++ Portfolio/SwiftUIComponents/Beginner/ConfirmationDialog/README.md
@@ -0,0 +1,3 @@
+#  ConfirmationDialog
+ConfirmationDialog은 하단에서 올라오는 듯한 버튼들과 제목, 내용을 포함한 정보를 사용자에게 전달할 수 있다. 얼럿이 화면 중앙에 노출되는 안내문구라면 액션시트는 하단에 슬라이딩되는 안내문구인 셈.
+- ActionSheet이 deprecated 됨에 따라서 이를 대체할 수 있는 ConfirmationDialog를 살펴보기로 하자.
