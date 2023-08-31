47c2267b8b8290042c87e26a01c61452a48d3f35
YunSwain <destap@naver.com>
2023-08-31 15:19:00 +0900

    PreferenceKey

---
+++ Portfolio/SwiftUIComponents/Advanced/PreferenceKey/PreferenceKeyBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  PreferenceKeyBootcamp.swift
+//  Portfolio
+//
+//  Created by Swain Yun on 2023/08/31.
+//
+
+import SwiftUI
+
+struct PreferenceKeyBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreferenceKeyBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferenceKeyBootcamp()
+    }
+}
