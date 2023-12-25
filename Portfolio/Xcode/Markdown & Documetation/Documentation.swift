7dc622fc1aa455d73c590e3e319e688ae8b4ddae
YunSwain <99116619+Remaked-Swain@users.noreply.github.com>
2023-12-25 18:17:51 +0900

    Update Documentation.swift

--- Portfolio/Xcode/Markdown & Documetation/Documentation.swift
+++ Portfolio/Xcode/Markdown & Documetation/Documentation.swift
@@ -1,10 +1,3 @@
-//
-//  MarkUp.swift
-//  Portfolio
-//
-//  Created by Swain Yun on 2023/08/10.
-//
-
 import Foundation
 
 /**
@@ -38,6 +31,36 @@ import Foundation
  - Note: 뭔가를 기록해야하는 것은 이곳에 적기 [마크업에 대한 추가 자료 링크](https://developer.apple.com/library/archive/documentation/Xcode/Reference/xcode_markup_formatting_ref/index.html#//apple_ref/doc/uid/TP40016497-CH2-SW1)
  
  - Version: 버전 정보에 대해 적기
+ 
+ - Precondition:
+ 
+ - Postcondition:
+ 
+ - Requires:
+ 
+ - Invariant:
+ 
+ - Complexity:
+ 
+ - Warning:
+ 
+ - Copyright:
+ 
+ - Authors:
+ 
+ - Date:
+ 
+ - SeeAlso:
+ 
+ - Since:
+ 
+ - Attention:
+ 
+ - Bug:
+ 
+ - Experiment:
+ 
+ - Remark:
  */
 struct Person {
     let name: String
@@ -47,3 +70,7 @@ struct Person {
         return Person(name: name, age: age)
     }
 }
+
+/**
+ 퀵헬프 대상 개체 밑에 작성하는 주석은 Description 부분에 표기됨.
+*/
